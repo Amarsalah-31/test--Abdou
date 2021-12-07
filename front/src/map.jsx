8c6034e91f4c9d084b6a1c18eb7139f95cd2477f
Amarsalah-31 <amarnawal22@gmail.com>
2021-12-07 15:46:48 +0100

    promier commit

---
+++ front/src/map.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import L from "leaflet";
+
+const Map = () => {
+
+  return (
+    <div>
+      <div id="map" />
+    </div>
+  )
+}
+
+export default Map;
